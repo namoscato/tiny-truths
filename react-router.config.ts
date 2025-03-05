4162818db7b694f075b36193fc034ab303aaef83
Nick Amoscato <nick@amoscato.com>
2025-03-04 20:26:10 -0500

    Set environment

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,10 +3,16 @@ import { sentryOnBuildEnd } from "@sentry/react-router";
 import { getEpisodeConfigs } from "./app/episodes/utils/getEpisodeConfigs";
 
 export default {
-  buildEnd: sentryOnBuildEnd,
+  buildEnd: createBuildEnd(),
   prerender,
   ssr: false,
 } satisfies Config;
+
+function createBuildEnd(): Config["buildEnd"] {
+  if (process.env.SENTRY_AUTH_TOKEN) {
+    return sentryOnBuildEnd;
+  }
+}
 
 async function prerender(): Promise<string[]> {
   const episodes = await getEpisodeConfigs();
