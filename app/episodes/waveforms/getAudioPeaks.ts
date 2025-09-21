5bcd46f1dc40e746ac70b3b6171142437a224034
Nick Amoscato <nick@amoscato.com>
2025-09-21 14:13:15 -0400

    Add episode 4 (#59)

--- app/episodes/waveforms/getAudioPeaks.ts
+++ app/episodes/waveforms/getAudioPeaks.ts
@@ -1,8 +1,9 @@
 import episode1 from "./episode1.json";
 import episode2 from "./episode2.json";
 import episode3 from "./episode3.json";
+import episode4 from "./episode4.json";
 
-const episodePeaks = [episode1, episode2, episode3];
+const episodePeaks = [episode1, episode2, episode3, episode4];
 
 export function getAudioPeaks(episodeNumber: number): number[][] {
   const peaks = episodePeaks[episodeNumber - 1];
